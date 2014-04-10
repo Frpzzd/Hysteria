aced1e7d6f32f36b94db3405834afb4b280a5045
james7132 <james.siqi.liu@gmail.com>
2014-04-09 22:02:50 -0400

    Demo Commit 1

--- Assets/Scripts/Danmaku Engine/Option.cs
+++ Assets/Scripts/Danmaku Engine/Option.cs
@@ -5,14 +5,23 @@ using DanmakuEngine.Core;
 
 public class Option : CachedObject
 {
-	private const float minAngle = -45;
-	private const float maxAngle = 45;
+	private float angle = 0f;
+
+	public void Update()
+	{
+		float px, py, x, y;
+		x = Transform.position.x;
+		y = Transform.position.y;
+		px = Player.PlayerTransform.position.x;
+		py = Player.PlayerTransform.position.y;
+		angle = Mathf.Atan2 (py - y, px - x) * (180f / Mathf.PI) - 90f;
+	}
 	
 	public PlayerShot Fire()
 	{
 		if(Player.Sensing)
 		{
-			return PlayerShotPool.Spawn (Transform.position, Quaternion.Euler(0,0, Random.Range(minAngle, maxAngle) / ((Player.Focused) ? 2 : 1)) , false);
+			return PlayerShotPool.Spawn (Transform.position, Quaternion.Euler(0,0, angle / ((Player.Focused) ? 2 : 1)) , false);
 		}
 		else
 		{
